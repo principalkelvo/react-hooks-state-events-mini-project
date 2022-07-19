934805a1362658f8ebe27c62fc8e9fb9246f0fa8
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-19 09:20:48 +0300

    tasklist done

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,14 @@
 import React from "react";
 
-function TaskList() {
-  return (
-    <div className="tasks">
-      {/* display a list of tasks using Task component */}
-    </div>
-  );
+function TaskList({ Task }) {
+  const tasks = Task.map(function (task) {
+
+    return <li key={task.index}>{task.text}</li>;
+  });
+  console.log(tasks);
+  
+      /* display a list of tasks using Task component */
+  return <div className="tasks">{tasks}</div>;
 }
 
 export default TaskList;
