5641b03707bce1dd2b74a6068f5a9d1f62e88fea
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-07-19 17:19:27 +0300

    completed

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -1,11 +1,15 @@
 import React from "react";
+import Task from "./Task";
 
-function TaskList({ Task }) {
-  const tasks = Task.map(function (task) {
-
-    return <li key={task.index}>{task.text}</li>;
-  });
-  console.log(tasks);
+function TaskList({ allTasks, deleteTask }) {
+  const tasks = allTasks.map((task)=> (    
+  <Task
+    key={task.text}
+    text={task.text}
+    category={task.category}
+    deleteTask={deleteTask}
+  />
+  ));
   
       /* display a list of tasks using Task component */
   return <div className="tasks">{tasks}</div>;
